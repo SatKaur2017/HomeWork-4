86efbcb00759b9bf0737b878012bab476f1d8ecc
SatKaur2017 <sainisatti80@gmail.com>
2017-12-04 12:02:30 -0500

    I added Hello Selenium message

--- Sample.java
+++ Sample.java
@@ -2,7 +2,9 @@ package SampleClass;
 
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World + Hello Selenium");
+        System.out.println("Hello World + Hello Selenium");
+        System.out.println("Hello World + Hello Selenium");
 
         }
 }
